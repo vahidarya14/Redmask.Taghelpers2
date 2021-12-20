fa9372177b73c71a7161bf148b1db958d235ebec
Vahid Aria <Vahid@ARIA-LAPTOP>
2021-12-20 22:52:44 +0330

    add taginput

---
+++ src/Redmask.Taghelpers/TagHelpers/TagInputFor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Redmask.Taghelpers.TagHelpers
+{
+    [HtmlTargetElement("TagInputFor", Attributes = "asp-for", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class TagInputForTagHelper : TagHelper
+    {
+        [HtmlAttributeName("asp-for")]
+        public ModelExpression Model { get; set; }
+
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.SetHtmlContent($@"
+    <input data-role='tagsinput' value='{Model.Model?.ToString().Replace(", ", ",") ?? ""}' id='{Model.Name}_2' />
+    <input value='@({Model.Model})' name='{Model.Name}' id='{Model.Name}' type='hidden' />
+    <script>
+        $('#{Model.Name}_2').on('itemAdded',   function (e) {{ $('[name={Model.Name}]').val($(this).tagsinput()[0].itemsArray.join(', ')); }});
+        $('#{Model.Name}_2').on('itemRemoved', function (e) {{ $('[name={Model.Name}]').val($(this).tagsinput()[0].itemsArray.join(', ')); }});
+    </script>
+");
+        }
+
+    }
+}
