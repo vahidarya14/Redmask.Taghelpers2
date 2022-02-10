a8d7da8aa2edf709a776315a84a337eacb8b3857
Vahid Aria <Vahid@ARIA-LAPTOP>
2022-02-10 12:08:14 +0330

    a

--- Web.Test/Startup.cs
+++ Web.Test/Startup.cs
@@ -17,7 +17,7 @@ namespace Web.Test
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
